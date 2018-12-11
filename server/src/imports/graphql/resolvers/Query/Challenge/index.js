b8de31f78de23f8a332b6cec6d20133f8f43365b
ClementeSerrano <clementeserranosutil@gmail.com>
2018-12-11 19:09:13 -0300

    challenges queries now working with token

--- server/src/imports/graphql/resolvers/Query/Challenge/index.js
+++ server/src/imports/graphql/resolvers/Query/Challenge/index.js
@@ -1,11 +1,28 @@
 import { Challenge } from "../../../../collections";
+import { requireAuth } from "../../../../services";
 
 const getChallenge = {
-  getChallenge: (_, { _id }) => Challenge.findById({ _id })
+  getChallenge: async (_, { _id }, { user }) => {
+    try {
+      await requireAuth(user);
+
+      Challenge.findById({ _id });
+    } catch (error) {
+      throw error;
+    }
+  }
 };
 
 const getChallenges = {
-  getChallenges: () => Challenge.find({}).sort({ createdAt: -1 })
+  getChallenges: async (_, args, { user }) => {
+    try {
+      await requireAuth(user);
+
+      return Challenge.find({}).sort({ createdAt: -1 });
+    } catch (error) {
+      throw error;
+    }
+  }
 };
 
 export { getChallenge, getChallenges };
---
+++ server/src/imports/services/index.js
@@ -0,0 +1,3 @@
+import requireAuth from "./auth";
+
+export { requireAuth };
